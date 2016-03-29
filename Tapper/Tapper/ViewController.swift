e182055d0a499dff3315e6a2a52f909f5387be03
oscarnieblafuentes <oscarniebla@me.com>
2016-03-29 05:25:42 -0700

    TAPPER GAME

---
+++ Tapper/Tapper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tapper
+//
+//  Created by Oscar Niebla Fuentes on 3/29/16.
+//  Copyright © 2016 Oscar Niebla Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //outlets
+    
+    @IBOutlet weak var tLOGO: UIImageView!
+    @IBOutlet weak var HMtapBTN: UITextField!
+    @IBOutlet weak var playmeBTN: UIButton!
+    
+    @IBOutlet weak var coinPRESS: UIButton!
+    @IBOutlet weak var tapLABL: UILabel!
+
+
+    @IBAction func letsPLAY(sender: UIButton) {
+    }
+
+
+}
